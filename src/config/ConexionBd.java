1a18a93fc494d60efaf4711dea11d6c4fe57ee7f
leonelborgesdev <leonel03t@gmail.com>
2024-01-15 23:32:58 -0400

    attributos conexionbd

---
+++ src/config/ConexionBd.java
@@ -0,0 +1,7 @@
+package config;
+
+public class ConexionBd {
+    private static final String jdbcUrl= "jdbc:h2:~/tarjetas_db";
+    private static final String user = "sa";
+    private static final String password="";
+}
