033a865a407a566598ee7335e1338cf99da1372e
leonelborgesdev <leonel03t@gmail.com>
2024-01-15 23:35:32 -0400

    function getconnection

--- src/config/ConexionBd.java
+++ src/config/ConexionBd.java
@@ -1,7 +1,20 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBd {
     private static final String jdbcUrl= "jdbc:h2:~/tarjetas_db";
     private static final String user = "sa";
     private static final String password="";
+    public static Connection getConnection(){
+        Connection connection=null;
+        try{
+            connection= DriverManager.getConnection(jdbcUrl,user,password);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
