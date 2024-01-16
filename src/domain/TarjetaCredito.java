48a883c5e0f03f07d7e1472deaaee56687cd7080
leonelborgesdev <leonel03t@gmail.com>
2024-01-15 23:38:49 -0400

    entiti tarjetacredito

---
+++ src/domain/TarjetaCredito.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class TarjetaCredito {
+    private String numero;
+    private String titular;
+
+    public TarjetaCredito(String numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+    }
+
+}
