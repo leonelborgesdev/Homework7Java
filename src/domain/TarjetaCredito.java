4c8cd8e1e25e135bd7e5edbd0878b0a0de44a466
leonelborgesdev <leonel03t@gmail.com>
2024-01-15 23:39:32 -0400

    getter and setter tarjetacredito

--- src/domain/TarjetaCredito.java
+++ src/domain/TarjetaCredito.java
@@ -9,4 +9,19 @@ public class TarjetaCredito {
         this.titular = titular;
     }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
 }
