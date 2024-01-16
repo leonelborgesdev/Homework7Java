949ba503cab77a9b5f96256961581e7b740007aa
leonelborgesdev <leonel03t@gmail.com>
2024-01-15 23:42:48 -0400

    getter and setter tarjeta de debito

--- src/domain/TarjetaDebito.java
+++ src/domain/TarjetaDebito.java
@@ -4,4 +4,24 @@ public class TarjetaDebito {
     private String numero;
     private String titular;
 
+    public TarjetaDebito(String numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
 }
