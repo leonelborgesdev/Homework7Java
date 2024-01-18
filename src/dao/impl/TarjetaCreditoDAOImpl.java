59f8c496c88f3cb78edc43bcd45ef45271c38745
leonelborgesdev <leonel03t@gmail.com>
2024-01-17 23:38:16 -0400

    TarjetaCreditoDaoImpl

---
+++ src/dao/impl/TarjetaCreditoDAOImpl.java
@@ -0,0 +1,18 @@
+package dao.impl;
+
+import dao.TarjetaCreditoDAO;
+import domain.TarjetaCredito;
+
+import java.util.List;
+
+public class TarjetaCreditoDAOImpl implements TarjetaCreditoDAO {
+    @Override
+    public TarjetaCredito getTarjetaCreditoById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<TarjetaCredito> getAllTarjetasCredito() {
+        return null;
+    }
+}
