929a3816483afddfc0193e08f4d665bd0938cf5e
leonelborgesdev <leonel03t@gmail.com>
2024-01-17 23:44:01 -0400

    TarjetaDebitoDaoImpl

---
+++ src/dao/impl/TarjetaDebitoDAOImpl.java
@@ -0,0 +1,18 @@
+package dao.impl;
+
+import dao.TarjetaDebitoDAO;
+import domain.TarjetaDebito;
+
+import java.util.List;
+
+public class TarjetaDebitoDAOImpl implements TarjetaDebitoDAO {
+    @Override
+    public TarjetaDebito getTarjetaDebitoById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<TarjetaDebito> getAllTarjetasDebito() {
+        return null;
+    }
+}
