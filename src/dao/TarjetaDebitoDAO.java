0848bb65015d1c7e703019aa1ada6e4ca8c06bdd
leonelborgesdev <leonel03t@gmail.com>
2024-01-17 23:35:12 -0400

    interface getalltarjetasdebito

--- src/dao/TarjetaDebitoDAO.java
+++ src/dao/TarjetaDebitoDAO.java
@@ -1,4 +1,10 @@
 package dao;
 
+import domain.TarjetaDebito;
+
+import java.util.List;
+
 public interface TarjetaDebitoDAO {
+    TarjetaDebito getTarjetaDebitoById(int id);
+    List<TarjetaDebito> getAllTarjetasDebito();
 }
