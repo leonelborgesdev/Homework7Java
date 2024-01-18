214a4ac604cbbc2a9b55e29e2e7ab58c28942c51
leonelborgesdev <leonel03t@gmail.com>
2024-01-17 23:47:15 -0400

    tarjetadebitodaoImpl getalltarjetasdebito

--- src/dao/impl/TarjetaDebitoDAOImpl.java
+++ src/dao/impl/TarjetaDebitoDAOImpl.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TarjetaDebitoDAOImpl implements TarjetaDebitoDAO {
@@ -36,6 +37,23 @@ public class TarjetaDebitoDAOImpl implements TarjetaDebitoDAO {
 
     @Override
     public List<TarjetaDebito> getAllTarjetasDebito() {
-        return null;
+        List<TarjetaDebito> tarjetas = new ArrayList<>();
+
+        try (Connection connection = ConexionBd.getConnection()) {
+            String sql = "SELECT numero, titular FROM TarjetasDebito";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String numero = resultSet.getString("numero");
+                String titular = resultSet.getString("titular");
+                tarjetas.add(new TarjetaDebito(numero, titular));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tarjetas;
     }
 }
