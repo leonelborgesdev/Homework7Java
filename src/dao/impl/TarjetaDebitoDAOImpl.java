91fd958c4dea58228d0f83be3255790aa558802d
leonelborgesdev <leonel03t@gmail.com>
2024-01-17 23:46:01 -0400

    tarjetadebitodaoimpl gettarjetadebitobyId

--- src/dao/impl/TarjetaDebitoDAOImpl.java
+++ src/dao/impl/TarjetaDebitoDAOImpl.java
@@ -2,12 +2,35 @@ package dao.impl;
 
 import dao.TarjetaDebitoDAO;
 import domain.TarjetaDebito;
+import config.ConexionBd;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TarjetaDebitoDAOImpl implements TarjetaDebitoDAO {
     @Override
     public TarjetaDebito getTarjetaDebitoById(int id) {
+        // Realiza la consulta SQL para obtener una tarjeta de débito por ID
+        // Crea un objeto TarjetaDebito y lo devuelve
+        try (Connection connection = ConexionBd.getConnection()) { // <--Configura la conexión a la base de datos H2 (jdbcUrl, user, password)
+            String sql = "SELECT numero, titular FROM TarjetasDebito WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String numero = resultSet.getString("numero");
+                String titular = resultSet.getString("titular");
+                return new TarjetaDebito(numero, titular);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
