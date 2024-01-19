47827b63b3f88443d25859c781f9c2eddf6dc5a5
leonelborgesdev <leonel03t@gmail.com>
2024-01-18 23:37:40 -0400

    getalltarjetasdebito

--- src/service/TarjetaService.java
+++ src/service/TarjetaService.java
@@ -26,4 +26,7 @@ public class TarjetaService {
     public TarjetaDebito getTarjetaDebitoById(int id){
         return tarjetaDebitoDAO.getTarjetaDebitoById(id);
     }
+    public List<TarjetaDebito> getAllTarjetasDebito(){
+        return tarjetaDebitoDAO.getAllTarjetasDebito();
+    }
 }
