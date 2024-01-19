89c6671eba016b6318f71193f790100f45162a52
leonelborgesdev <leonel03t@gmail.com>
2024-01-18 23:32:09 -0400

    tarjetaservice constructor

--- src/service/TarjetaService.java
+++ src/service/TarjetaService.java
@@ -1,4 +1,16 @@
 package service;
 
+import dao.TarjetaCreditoDAO;
+import dao.TarjetaDebitoDAO;
+import dao.impl.TarjetaCreditoDAOImpl;
+import dao.impl.TarjetaDebitoDAOImpl;
+
 public class TarjetaService {
+    private final TarjetaCreditoDAO tarjetaCreditoDAO;
+    private final TarjetaDebitoDAO tarjetaDebitoDAO;
+
+    public TarjetaService() {
+        this.tarjetaCreditoDAO = new TarjetaCreditoDAOImpl();
+        this.tarjetaDebitoDAO = new TarjetaDebitoDAOImpl();
+    }
 }
