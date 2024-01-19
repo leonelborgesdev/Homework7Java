ceaed38fa5f4199df60976a27c1eab2171cf926f
leonelborgesdev <leonel03t@gmail.com>
2024-01-18 23:34:07 -0400

    gettarjetacreditobyid

--- src/service/TarjetaService.java
+++ src/service/TarjetaService.java
@@ -4,6 +4,7 @@ import dao.TarjetaCreditoDAO;
 import dao.TarjetaDebitoDAO;
 import dao.impl.TarjetaCreditoDAOImpl;
 import dao.impl.TarjetaDebitoDAOImpl;
+import domain.TarjetaCredito;
 
 public class TarjetaService {
     private final TarjetaCreditoDAO tarjetaCreditoDAO;
@@ -12,5 +13,8 @@ public class TarjetaService {
     public TarjetaService() {
         this.tarjetaCreditoDAO = new TarjetaCreditoDAOImpl();
         this.tarjetaDebitoDAO = new TarjetaDebitoDAOImpl();
+    }
+    public TarjetaCredito getTarjetaCreditoById(int id){
+        return tarjetaCreditoDAO.getTarjetaCreditoById(id);
     }
 }
