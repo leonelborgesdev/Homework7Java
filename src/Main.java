c5ea8078ca52b5557f26d796c55d1443ec34247c
leonelborgesdev <leonel03t@gmail.com>
2024-01-18 23:45:49 -0400

    tarjetas main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import domain.TarjetaCredito;
+import domain.TarjetaDebito;
 import service.TarjetaService;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -16,6 +19,17 @@ public class Main {
             System.out.println("Titular: "+tarjetaCredito.getTitular());
         }else{
             System.out.println("Tarjeta de Credito no encontrada.");
+        }
+
+        List<TarjetaDebito> tarjetaDebito= tarjetaService.getAllTarjetasDebito();
+        if (!tarjetaDebito.isEmpty()){
+            System.out.println("\nTarjetas de Debito encontradas:");
+            for (TarjetaDebito tarjeta : tarjetaDebito){
+                System.out.println("Numero: "+tarjeta.getNumero());
+                System.out.println("Titular: "+tarjeta.getTitular());
+            }
+        }else{
+            System.out.println("\nNo se encontraron Tarjetas de debito");
         }
     }
 }
